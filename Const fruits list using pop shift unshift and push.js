723668031f6af550e67311b66961a2d6c6cc7875
Thecodetree <45953507+Thecodetree@users.noreply.github.com>
2022-04-05 16:43:38 -0500

    Const list using pop push shift and unshift methods
    This is a list using a const array along with .pop, .push, .shift and .unshift methods

---
+++ Const fruits list using pop shift unshift and push.js
@@ -0,0 +1,10 @@
+const fruits = ['Guava', 'Peaches', 'Bananas', 'Watermelons', 'Mangos'];
+fruits.push('papaya', 'blueberries');
+
+fruits.pop();
+
+fruits.shift();
+
+fruits.unshift('Blackberries', 'Strawberries');
+
+console.log(fruits);
