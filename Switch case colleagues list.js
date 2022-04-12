caddc40881dd014d5cfdf6651749775eac6e4517
The code tree <45953507+Thecodetree@users.noreply.github.com>
2022-04-12 17:59:56 -0500

    Switch/case colleagues list
    This is a switch case colleagues list

---
+++ Switch case colleagues list.js
@@ -0,0 +1,11 @@
+const colleaguesList = ['Johnnie', 'Boots', 'Harper', 'Boonie', 'Bobbie', 'LT', 'Swammy', 'Jazz'];
+
+switch (colleaguesList.indexof) {
+  case ("Johnnie is 4"):
+    break;
+
+  case ("Boots is 16"):
+    break;
+}
+
+console.log('Johnnie');
