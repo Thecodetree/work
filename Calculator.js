93df602011d979363d94d0128b04227cd83de11c
The code tree <45953507+Thecodetree@users.noreply.github.com>
2024-01-30 14:55:11 -0600

    A calculator in JS, CSS & HTML
    This is the code for a calculator in JavaScript, CSS and HTML

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+let result = "";
+
+function clearResult() {
+    result = "";
+    document.getElementById("result").value = result;}
+
+    function appendToResult(value) {
+        result += value;
+
+        document.getElementById("result").value = result;}
+
+        function calculateResult() {
+            try {
+                result = eval(result);
+                document.getElementById("result").value = result;}
+                catch (error) {
+                    document.getElementById("result").value = "error";
+                }
+        }
