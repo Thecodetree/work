741c0dab25f24bca5278130d80c6a6326008ad47
The code tree <45953507+Thecodetree@users.noreply.github.com>
2024-04-10 14:18:18 -0500

    CSS/HTML/JavaScript Fade/Slide example
    This is an example of fade and slide written in CSS, HTML & JavaScript

---
+++ CSS Fade and Slide Example.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {    
+    const container = document.querySelector(
+        '.fade-slide-container');
+    
+        container.classList.add('show');
+})
