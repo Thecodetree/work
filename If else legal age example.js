b23baf8cca97451e2992c9630f573f69f60bbf3c
The code tree <45953507+Thecodetree@users.noreply.github.com>
2024-04-29 14:23:47 -0500

    Legal age barometer using if else staments
    This is a barometer for determining a leg al age and the message given to each using if/else statements in JavaScript.

---
+++ If else legal age example.js
@@ -0,0 +1,13 @@
+// These are examples of if, else statements
+
+let greeting1 = "greeting";
+
+let greeting2 = 'greeting';
+
+var Age1 = "20"
+
+var age = '-=21'
+
+if (Age1 > 20) {greeting1 = "welcome to club curtis";
+}
+else (age -= 21); {greeting2 = "Come back at legal age"}
