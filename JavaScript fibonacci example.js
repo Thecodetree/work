266037d54e1f0fb6f5e86cb94e7d632cd98bcac5
The code tree <45953507+Thecodetree@users.noreply.github.com>
2024-05-06 13:39:49 -0500

    JavaScript Fibonacci example
    This is an example of Fibonacci in the JavaScript programming language

---
+++ JavaScript fibonacci example.js
@@ -0,0 +1,12 @@
+function generateFibonacci(n) {
+    let sequence = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        sequence[i] = 
+        sequence[i - 1] +
+        sequence[i - 2];}
+
+        return sequence;
+}
+
+console.log(generateFibonacci(12));
